b13d298e14021f2b41497908dd7971a2e5f94663
Riza Fahmi <rizafahmi@gmail.com>
2017-03-28 12:28:20 +0700

    Add provider and extract reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './App'
 import './index.css'
+import { counterReducer } from './reducers'
+
+const store = createStore(counterReducer)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}><App /></Provider>,
   document.getElementById('root')
 )
